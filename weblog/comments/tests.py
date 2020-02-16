9b38cfdbadc2b1ed0aebe3296aa15d31a9794eb5
Adriaan Coetzee <ascendedcrow@gmail.com>
2020-02-16 21:45:38 +0100

    Added some tests

--- weblog/comments/tests.py
+++ weblog/comments/tests.py
@@ -1,3 +1,38 @@
+import django
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from comments.models import Comment
+from posts.models import Post
+from django.db import transaction
+
+class CommentTest(TestCase):
+    """Tests for the application views."""
+    if django.VERSION[:2] >= (1, 7):
+        # Django 1.7 requires an explicit setup() when running tests in PTVS
+        @classmethod
+        def setUpClass(cls):
+            super(CommentTest, cls).setUpClass()
+            django.setup()
+
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+    # Function Tests
+    def test_functions(self):
+        comment = Comment()
+        User = get_user_model()
+        user = User.objects.first()
+        with transaction.atomic():
+            post = Post()
+            post.title="Test"
+            post.user = user
+           
+            post.save();
+            comment.post = post
+            comment.save()    
+
+            comment.approve_comment()
+
+        self.assertEqual(comment.approved, True)
--- weblog/posts/tests.py
+++ weblog/posts/tests.py
@@ -1,3 +1,45 @@
+import django
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from posts.models import Post
+from django.db import transaction
+
+class PostTest(TestCase):
+    """Tests for the application views."""
+    if django.VERSION[:2] >= (1, 7):
+        # Django 1.7 requires an explicit setup() when running tests in PTVS
+        @classmethod
+        def setUpClass(cls):
+            super(PostTest, cls).setUpClass()
+            django.setup()
+
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+    # Models Tests
+    def test_posts(self):
+        post = Post()
+        with transaction.atomic():
+            with self.assertRaises(django.db.utils.IntegrityError):
+                post.save()
+
+        User = get_user_model()
+        user = User.objects.first()
+
+        with transaction.atomic():
+            with self.assertRaises(django.db.utils.IntegrityError):
+                # Duplicate check
+                post = Post()
+                post.title="Test"
+                post.user = user
+           
+                post.save();
+
+                post = Post()
+                post.title="Test"
+                post.user = user
+           
+                post.save();
+        
