d383e693b2c653b1a7d88bc6d01dbd7abc0693ae
Adriaan Coetzee <ascendedcrow@gmail.com>
2020-02-16 22:39:17 +0100

    Added postgres

--- weblog/weblog/settings.py
+++ weblog/weblog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'weblog.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weblog',
+        'USER': 'weblog_user',
+        'PASSWORD': 'weblog_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
